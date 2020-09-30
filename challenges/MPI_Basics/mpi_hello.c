01e0bb3360d4f4e858e79a21b3c52cab2b4aaf07
Suzanne Parete-Koon <nk8@login1.ascent.olcf.ornl.gov>
2020-09-30 17:21:12 -0400

    fixed Hello

--- challenges/MPI_Basics/mpi_hello.c
+++ challenges/MPI_Basics/mpi_hello.c
@@ -5,21 +5,40 @@ int main(int argc, char **argv)
 {
     int rank, size;
 
-    // Initialize MPI
+    /*----------------*/
+    /* Initialize MPI */
+    /*----------------*/
 
-     MPI_Init(&argc, &argv);  
+     MPI_Init(&argc, &argv);
 
-    //Get the number of ranks (size) from the Communicator  
-     
-     MPI_Comm_size(MPI_COMM_WORLD, &size);     
+    /*------------------------------------------------------*/
+    /* Get the number of ranks (size) from the Communicator */
 
- 
-    //Print hello from each rank 
+    /*------------------------------------------------------*/
+
+     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    /*------------------------------------------------------*/
+    /* Get the Rank ID for each process                     */
+    /* Fix the code here.                                   */
+    /*------------------------------------------------------*/
+
+
+
+
+    /*------------------------------------------------------*/
+    /* Print hello from each rank                           */
+    /*------------------------------------------------------*/
 
      printf("Hello from rank %d of %d total \n", rank, size);
-      
-    //Clean up and finalize the MPI environment
+
+    /*------------------------------------------------------*/
+    /* Clean up and finalize the MPI environment            */
+    /*------------------------------------------------------*/
 
      MPI_Finalize();
+
     return 0;
 }
+
